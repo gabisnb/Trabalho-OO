0ed85647ea32d7a800366fb301b202ea44b81f2e
João Victor Corrêa de Almeida <joaovictordealmeid@gmail.com>
2023-04-24 14:19:28 -0300

    Adicionando projeto

---
+++ Trabalho/src/main/java/dcc025/trabalho/Trabalho.java
@@ -0,0 +1,11 @@
+package dcc025.trabalho;
+
+/**
+ * @author joaov
+ */
+public class Trabalho {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
