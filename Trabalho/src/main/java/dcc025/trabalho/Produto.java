f147fc2590e8d5304264497c95dd5c8ab66ed635
Eduarda Araujo <74570744+eduardaac@users.noreply.github.com>
2023-05-06 16:51:56 -0300

    Update Produto.java

--- Trabalho/src/main/java/dcc025/trabalho/Produto.java
+++ Trabalho/src/main/java/dcc025/trabalho/Produto.java
@@ -6,10 +6,6 @@ package dcc025.trabalho;
 import java.util.*;
 import dcc025.trabalho.ListaQuantidadeCor.Cor;
 
-/**
- *
- * @author joaov
- */
 public class Produto implements TiposProdutos{
     private double preco;
     private int quantidadeTotal;
