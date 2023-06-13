2b62bbb6ead26f827a95fef361bccb04a853fa50
Gabriel Oliveira Quaresma <gabriel.oliveira.quaresma@gmail.com>
2023-06-12 22:26:15 -0300

    Iniciando banco de dados

---
+++ Trabalho/src/main/java/dcc025/trabalho/persistence/Arquivo.java
@@ -0,0 +1,27 @@
+package dcc025.trabalho.persistence;
+import java.io.*;
+
+public class Arquivo {
+    public static String le(String filePath) {
+        StringBuilder content = new StringBuilder();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                content.append(line).append("\n");
+            }
+        } catch (IOException e) {
+            //e.printStackTrace();
+        }
+
+        return content.toString();
+    }
+
+    public static void salva(String filePath, String content) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath, false))) {
+            writer.write(content);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ Trabalho/src/main/java/dcc025/trabalho/persistence/CompradorPersistence.java
@@ -0,0 +1,45 @@
+package dcc025.trabalho.persistence;
+
+import dcc025.trabalho.Usuario.Comprador;
+import dcc025.trabalho.Usuario.Vendedor;
+
+import java.io.File;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+import com.google.gson.Gson;
+
+public class CompradorPersistence implements Persistence<Comprador> {
+
+    private static final String PATH = DIRECTORY + File.separator + "compradores.json";
+    @Override
+    public void save(List<Comprador> itens){
+        Gson gson = new Gson();
+        String json = gson.toJson(itens);
+
+        File diretorio = new File(DIRECTORY);
+        if(!diretorio.exists())
+            diretorio.mkdirs();
+
+        Arquivo.salva(PATH, json);
+    }
+
+    @Override
+    public List<Comprador> findAll() {
+        Gson gson = new Gson();
+
+        String json = Arquivo.le(PATH);
+
+        List<Vendedor> compradores = new ArrayList<>();
+        if(!json.trim().equals("")) {
+
+            Type tipoLista = new TypeToken<List<Vendedor>>() {
+            }.getType();
+            compradores = gson.fromJson(json, tipoLista);
+
+            if (compradores == null)
+                compradores = new ArrayList<>();
+        }
+
+        return compradores;
+}
