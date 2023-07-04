ba1c039bf3172600cb0dda8d42df38b92693a7c0
Gabriela Singulani <gabriela.singulani@ice.ufjf.br>
2023-07-04 07:54:25 -0300

    Update Main.java

--- Trabalho/src/main/java/dcc025/trabalho/Main.java
+++ Trabalho/src/main/java/dcc025/trabalho/Main.java
@@ -9,77 +9,25 @@ public class Main {
 
     public static void main(String[] args) {
         
-//       VendedorPersistence persistence =  new VendedorPersistence();
-//        List<Vendedor> list = new ArrayList();
-//        list.add(new Vendedor("t", "t", "t"));
-//
-//        persistence.save(list);
-//        list.add(new Vendedor("a", "a", "a"));
-//        persistence.save(list);
-
-//        CompradorPersistence cpersistence =  new CompradorPersistence();
-//        List<Comprador> clist = new ArrayList();
-//        clist.add(new Comprador("t", "t", "t", 100.00));
-//        cpersistence.save(clist);
-//        clist.add(new Comprador("a", "a", "a", 1000.00));
-//        cpersistence.save(clist);
-//
+        VendedorPersistence persistence =  new VendedorPersistence();
+        if(persistence.findAll()==null){
+            List<Vendedor> list = new ArrayList();
+            list.add(new Vendedor("t", "t", "t"));
+            list.add(new Vendedor("a", "a", "a"));
+            persistence.save(list);
+        }
+        
+        CompradorPersistence cpersistence =  new CompradorPersistence();
+        if(cpersistence.findAll()==null){
+            List<Comprador> clist = new ArrayList();
+            clist.add(new Comprador("t", "t", "t", 100.00));
+            clist.add(new Comprador("a", "a", "a", 1000.00));
+            cpersistence.save(clist);
+        }
+        
         TelaLogin login = new TelaLogin();
         login.desenha();
-//        TelaCompra compra = new TelaCompra(login, new Comprador("teste", "teste", "teste"));
-//        compra.desenha();
-//        TelaVende vende = new TelaVende(null, new Vendedor("teste", "teste", "teste"));
-//        vende.desenha();
-//        TelaCarrinho tela = new TelaCarrinho(new Comprador("teste", "teste", "teste"));
-//        tela.desenha();
-        
-        /* Scanner teclado = new Scanner(System.in);
-        int tipoUsuario; 
-        String nome, email, senha;
-        
-        System.out.println("");
-        System.out.println("--LOGIN--");
-        System.out.print("Nome: ");
-        nome = teclado.next();
-        System.out.print("Email: ");
-        email = teclado.next();
-        System.out.print("Senha: ");
-        senha = teclado.next();
-        System.out.println("");
-        System.out.println("--TIPO DE USUÁRIO--");
-        System.out.println("1 - Vendedor");
-        System.out.println("2 - Comprador");
-        tipoUsuario = teclado.nextInt();
-        
-        Produto eletrodomestico = new Produto(3000, 5, Cor.AZUL_CLARO, "Computador");
-        
-        
-        switch(tipoUsuario){
-            case 1:
-                Vendedor vendedor = new Vendedor(nome, email, senha);
-                vendedor.DadosVendedor();
-                vendedor.adicionarProduto(roupa);
-                roupa.addCorProduto(12, Cor.ROSA);
-                vendedor.adicionarProduto(movel);
-                vendedor.adicionarProduto(material);
-                vendedor.adicionarProduto(eletrodomestico);
-                vendedor.exibirEstoque();
-                break;
-            
-            case 2:
-                Comprador comprador = new Comprador(nome, email, senha);
-                comprador.DadosComprador();
-                comprador.adicionarCarrinho(roupa, 1);
-                comprador.adicionarCarrinho(movel, 1);
-                comprador.adicionarCarrinho(material, 2);
-                comprador.adicionarCarrinho(eletrodomestico, 5);
-                comprador.exibirCarrinho();
-                break;
-            
-            default:
-                System.out.println("Opção de usuário inválido");
-                break;
-        */}       
-    }
+    }       
+}
 
 
