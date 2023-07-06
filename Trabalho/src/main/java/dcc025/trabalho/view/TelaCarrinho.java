2ec4c50c84ceffb86feba74ddf1092e328eb2c11
Eduarda Araujo <74570744+eduardaac@users.noreply.github.com>
2023-07-05 22:47:40 -0300

    Botões estilizados

--- Trabalho/src/main/java/dcc025/trabalho/view/TelaCarrinho.java
+++ Trabalho/src/main/java/dcc025/trabalho/view/TelaCarrinho.java
@@ -6,91 +6,95 @@ import java.awt.*;
 import java.util.*;
 import javax.swing.*;
 
-public class TelaCarrinho extends Tela{
-    
+public class TelaCarrinho extends Tela {
+
     private Comprador usuario;
-    
     private TelaComprador telaComp;
-    
     private JList<Produto> jlistProdutos;
 
     protected TelaCarrinho(Comprador comp, TelaComprador tela) {
         usuario = comp;
         telaComp = tela;
-        super.botoes = new ArrayList();
-        super.labels = new ArrayList();
+        super.botoes = new ArrayList<>();
+        super.labels = new ArrayList<>();
     }
-    
-    public void desenha(){
+
+    public void desenha() {
         tela = new JFrame();
         tela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         tela.setSize(LARGURA, ALTURA);
         tela.setLocationRelativeTo(null);
         tela.setVisible(true);
         tela.setLayout(new BorderLayout());
-        
+
         desenhaMenu();
-        try{
+        try {
             carregaCarrinhoBanco(usuario.getProdutos());
+        } catch (NullPointerException e) {
+            System.out.println("Carrinho vazio!");
         }
-        catch(NullPointerException e){System.out.println("Carrinho vazio!");}
-        
+
         tela.pack();
     }
-    
-    private void desenhaMenu(){
+
+    private void desenhaMenu() {
         JPanel painel = configuraPainelMain("Carrinho de Compras");
-        
+
         labels.add(new JLabel("Nome: " + usuario.getNome()));
         labels.add(new JLabel("Email: " + usuario.getEmail()));
         labels.add(new JLabel("Saldo: " + df.format(usuario.getSaldo())));
         labels.add(new JLabel("Valor Total: " + df.format(usuario.getCarrinho().getTotalPagar())));
-                
-        //Botão Comprar
+
+        // Botão Comprar
         botoes.add(new JButton("Comprar"));
         botoes.get(0).addActionListener((java.awt.event.ActionEvent e) -> {
             abrirPagamento();
         });
-        
-        //Botão Voltar
+        botoes.get(0).setBackground(Color.decode("#002847"));
+        botoes.get(0).setForeground(Color.WHITE);
+
+        // Botão Remover do Carrinho
         botoes.add(new JButton("Remover do Carrinho"));
         botoes.get(1).addActionListener((java.awt.event.ActionEvent e) -> {
-            try{
+            try {
                 usuario.removeProdutoCarrinho(jlistProdutos.getSelectedValue().getProduct_id(), 1);
-            }
-            catch(NullPointerException e1){
+            } catch (NullPointerException e1) {
                 JOptionPane.showMessageDialog(null, "Selecione o produto que deseja remover!");
             }
             carregaCarrinhoBanco(usuario.getProdutos());
             telaComp.salvar();
         });
-        
+        botoes.get(1).setBackground(Color.decode("#002847"));
+        botoes.get(1).setForeground(Color.WHITE);
+
         JPanel painelAux = new JPanel();
         painelAux.add(desenhaLabel(labels));
         painelAux.add(desenhaLista("Produtos no Carrinho"));
         painelAux.add(desenhaBotoes(botoes));
         painel.add(painelAux, BorderLayout.CENTER);
 
-        //Botão Voltar
+        // Botão Voltar
         botoes.add(new JButton("Voltar"));
         botoes.get(2).addActionListener((java.awt.event.ActionEvent e) -> {
             tela.dispose();
             telaComp.abrir();
         });
-        
+        botoes.get(2).setBackground(Color.decode("#002847"));
+        botoes.get(2).setForeground(Color.WHITE);
+
         JPanel bpainel = new JPanel();
         bpainel.add(botoes.get(2));
         painel.add(bpainel, BorderLayout.PAGE_END);
-        
+
         tela.getContentPane().add(painel, BorderLayout.CENTER);
     }
-    
+
     @Override
-    protected JPanel desenhaLista(String string){
+    protected JPanel desenhaLista(String string) {
 
         JPanel painel = new JPanel();
         painel.setBorder(BorderFactory.createTitledBorder(string));
-        painel.setPreferredSize(new Dimension(LARGURA, ALTURA/3));
+        painel.setPreferredSize(new Dimension(LARGURA, ALTURA / 3));
         painel.setLayout(new BorderLayout());
 
         DefaultListModel<Produto> model = new DefaultListModel<>();
@@ -100,9 +104,9 @@ public class TelaCarrinho extends Tela{
         painel.add(new JScrollPane(jlistProdutos), BorderLayout.CENTER);
         return painel;
     }
-    
-    protected void abrirPagamento(){
-        if(this.usuario.getCarrinho().isEmpty()){
+
+    protected void abrirPagamento() {
+        if (this.usuario.getCarrinho().isEmpty()) {
             JOptionPane.showMessageDialog(null, "Carrinho vazio!");
             return;
         }
@@ -110,23 +114,23 @@ public class TelaCarrinho extends Tela{
         telaPaga.desenha();
         tela.setVisible(false);
     }
-    
-    public void carregaCarrinhoBanco(java.util.List<Produto> carrinho){
-        DefaultListModel<Produto> model = (DefaultListModel<Produto>)jlistProdutos.getModel();
+
+    public void carregaCarrinhoBanco(java.util.List<Produto> carrinho) {
+        DefaultListModel<Produto> model = (DefaultListModel<Produto>) jlistProdutos.getModel();
         model.clear();
-        for (Produto c: carrinho) {
+        for (Produto c : carrinho) {
             model.addElement(c);
         }
         labels.get(3).setText("Valor Total: " + df.format(usuario.getCarrinho().getTotalPagar()));
     }
-    
-    protected void abrir(){
+
+    protected void abrir() {
         tela.setVisible(true);
         carregaCarrinhoBanco(usuario.getProdutos());
         telaComp.salvar();
     }
-    
-    public void recarregaSaldo(){
+
+    public void recarregaSaldo() {
         labels.get(2).setText("Saldo: " + df.format(usuario.getSaldo()));
         telaComp.carrega();
     }
